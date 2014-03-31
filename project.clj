208f5f604bc38dd2d9e529b2f6580a6f0b74d3d6
Christian Berg <berg.christian@gmail.com>
2014-03-31 22:56:34 +0200

    Add markdown rendering.

--- project.clj
+++ project.clj
@@ -3,6 +3,8 @@
   :url "http://christianberg.github.io/"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [stasis "1.0.0"]
-                 [ring "1.2.2"]]
+                 [ring "1.2.2"]
+                 [hiccup "1.0.5"]
+                 [me.raynes/cegdown "0.1.1"]]
   :ring {:handler christianberg.web/app}
   :profiles {:dev {:plugins [[lein-ring "0.8.10"]]}})
--- src/christianberg/web.clj
+++ src/christianberg/web.clj
@@ -1,7 +1,24 @@
 (ns christianberg.web
-  (:require [stasis.core :as stasis]))
+  (:require [clojure.string :as str]
+            [hiccup.page :refer [html5]]
+            [me.raynes.cegdown :as md]
+            [stasis.core :as stasis]))
+
+(defn layout-page [content]
+  (html5
+   [:head
+    [:title "Christian Bergs Blog"]]
+   [:body content]))
+
+(defn markdown-pages [pages]
+  (zipmap (map #(str/replace % #"\.md$" "") (keys pages))
+          (map #(layout-page (md/to-html %)) (vals pages))))
 
 (defn get-pages []
-  (stasis/slurp-directory "resources/public" #".*\.(html|css|js)$"))
+  (stasis/merge-page-sources
+   {:public
+    (stasis/slurp-directory "resources/public" #".*\.(html|css|js)$")
+    :markdown
+    (markdown-pages (stasis/slurp-directory "resources/md" #"\.md$"))}))
 
 (def app (stasis/serve-pages get-pages))
