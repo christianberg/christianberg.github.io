8ea1d6a3138dff0c280bb1d2f3985e57bf853e7f
Christian Berg <berg.christian@gmail.com>
2014-04-23 16:10:47 +0200

    Minimal layout for blog posts.
    Markdown posts are rendered as HTML. Layout includes a navigation bar
    with link back to the home page and a footer with copyright.
    
    [finishes #69694096]

--- src/christianberg/web.clj
+++ src/christianberg/web.clj
@@ -2,17 +2,47 @@
   (:require [clojure.string :as str]
             [hiccup.page :refer [html5]]
             [me.raynes.cegdown :as md]
-            [stasis.core :as stasis]))
+            [stasis.core :as stasis])
+  (:import [java.util Calendar]))
+
+(defn current-year []
+  (.. Calendar
+      getInstance
+      (get Calendar/YEAR)))
 
 (defn layout-page [content]
   (html5
    [:head
+    [:meta
+     {:name "viewport"
+      :content "width=device-width, initial-scale=1"}]
+    [:link
+     {:rel "stylesheet"
+      :href "//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css"}]
     [:title "Christian Bergs Blog"]]
-   [:body content]))
+   [:body
+    [:div.navbar.navbar-default
+     [:div.container
+      [:div.navbar-header
+       [:a.navbar-brand {:href "/"}
+        "christianberg.github.io"]]]]
+    content
+    [:footer
+     [:div.container
+      [:p.text-muted
+       [:small (str "&copy; 2009-" (current-year) " Christian Berg")]]]]]))
+
+(defn layout-post [content]
+  (layout-page
+   [:div.container
+    content]))
 
 (defn markdown-pages [pages]
-  (zipmap (map #(str/replace % #"\.md$" "") (keys pages))
-          (map #(layout-page (md/to-html %)) (vals pages))))
+  (into {}
+        (for [[path content] pages]
+          (let [stripped-path (str/replace path #"\.md$" "")]
+            [stripped-path
+             (layout-post (md/to-html content))]))))
 
 (defn get-pages []
   (stasis/merge-page-sources
