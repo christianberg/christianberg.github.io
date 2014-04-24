70cd19f1ff4ca588049c9277e3b9928066083c6d
Christian Berg <berg.christian@gmail.com>
2014-04-24 15:50:21 +0200

    Add clj-time dependency.

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
                  [stasis "1.0.0"]
                  [ring "1.2.2"]
                  [hiccup "1.0.5"]
-                 [me.raynes/cegdown "0.1.1"]]
+                 [me.raynes/cegdown "0.1.1"]
+                 [clj-time "0.6.0"]]
   :ring {:handler christianberg.web/app}
   :aliases {"build-site" ["run" "-m" christianberg.web/export]}
   :profiles {:dev {:plugins [[lein-ring "0.8.10"]]}})
