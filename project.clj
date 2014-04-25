ccbecc0ac032f04b52c65baf4ca4b22fc4aea311
Christian Berg <berg.christian@gmail.com>
2014-04-25 11:56:57 +0200

    Add Atom feed.
    [finishes #69694098]

--- project.clj
+++ project.clj
@@ -6,7 +6,8 @@
                  [ring "1.2.2"]
                  [hiccup "1.0.5"]
                  [me.raynes/cegdown "0.1.1"]
-                 [clj-time "0.6.0"]]
+                 [clj-time "0.6.0"]
+                 [org.clojure/data.xml "0.0.7"]]
   :ring {:handler christianberg.web/app}
   :aliases {"build-site" ["run" "-m" christianberg.web/export]}
   :profiles {:dev {:plugins [[lein-ring "0.8.10"]]}})
--- src/christianberg/web.clj
+++ src/christianberg/web.clj
@@ -4,11 +4,15 @@
             [me.raynes.cegdown :as md]
             [stasis.core :as stasis]
             [clj-time.core :as t]
-            [clj-time.format :as tf]))
+            [clj-time.format :as tf]
+            [clojure.data.xml :as xml]))
 
 (defn date-from-path [path]
   (tf/parse (tf/formatter "yyyy/MM/dd")
             (second (first (re-seq #"(\d{4}/\d{2}/\d{2})" path)))))
+
+(defn slug-from-path [path]
+  (second (first (re-seq #"\d{4}/\d{2}/\d{2}/(.*)\.md" path))))
 
 (defn current-year []
   (t/year (t/now)))
@@ -19,16 +23,25 @@
     [:meta
      {:name "viewport"
       :content "width=device-width, initial-scale=1"}]
+    [:meta
+     {:name "author"
+      :content "Christian Berg"}]
     [:link
      {:rel "stylesheet"
       :href "//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css"}]
+    [:link
+     {:rel "alternate"
+      :href "/atom.xml"
+      :title "Christian Bergs Blog"
+      :type "application/atom+xml"}]
     [:title "Christian Bergs Blog"]]
    [:body
     [:div.navbar.navbar-default
      [:div.container
       [:div.navbar-header
-       [:a.navbar-brand {:href "/"}
-        "christianberg.github.io"]]
+       [:a.navbar-brand {:href "/"
+                         :title "{:author \"Christian Berg\"\n :type :blog}"}
+        "(christianberg.github.io)"]]
       [:a.navbar-text.navbar-right
        {:href "/archive"}
        "Archive"]]]
@@ -49,11 +62,16 @@
                                            content)))
           references (first
                       (re-seq #"(?ms)\[\d+\]:.*"
-                              content))]
+                              content))
+          date (date-from-path path)
+          slug (slug-from-path path)
+          id (str (tf/unparse (tf/formatters :date) date) "-" slug)]
       {:path path
        :url (str/replace path #"\.md$" "")
+       :id id
+       :slug slug
        :title title
-       :date (date-from-path path)
+       :date date
        :source content
        :html (md/to-html content [:fenced-code-blocks])
        :first-paragraph-source first-paragraph-source
@@ -85,7 +103,7 @@
         {:href (:url post)}
         "more &raquo;"]
        [:hr]])
-    [:a.btn.btn-default {:href "/archive"} "See all posts"]]))
+    [:a.btn.btn-default {:href "/archive"} "Blog Archive"]]))
 
 (defn archive-page []
   (let [posts-by-year (group-by #(t/year (:date %)) (posts))]
@@ -111,12 +129,32 @@
     [:meta {:http-equiv "refresh"
             :content (str "0; url=" url)}]]))
 
+(defn- entry [post]
+  [:entry
+   [:title (:title post)]
+   [:updated (:date post)]
+   [:author [:name "Christian Berg"]]
+   [:link {:href (str "http://christianberg.github.io" (:url post))}]
+   [:id (str "urn:christianberg-github-io:feed:post:" (:id post))]
+   [:content {:type "html"} (:html post)]])
+
+(defn atom-xml [posts]
+  (xml/emit-str
+   (xml/sexp-as-element
+    [:feed {:xmlns "http://www.w3.org/2005/Atom"}
+     [:id "urn:christianberg-github-io:feed"]
+     [:updated (-> posts first :date)]
+     [:title {:type "text"} "Christian Bergs Blog"]
+     [:link {:rel "self" :href "http://christianberg.github.io/atom.xml"}]
+     (map entry posts)])))
+
 (defn get-pages []
   (stasis/merge-page-sources
    {:index
     {"/" (index-page)
      "/archive" (archive-page)
-     "/blog/archives" (redirect-to "/archive")}
+     "/blog/archives" (redirect-to "/archive")
+     "/atom.xml" (atom-xml (reverse (sort-by :date (posts))))}
     :public
     (stasis/slurp-directory "resources/public" #".*\.(html|css|js)$")
     :markdown
