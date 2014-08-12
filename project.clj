34843cf33c018bee96b87d41a61de89e0bbca912
Christian Berg <berg.christian@gmail.com>
2014-08-12 23:47:29 +0200

    Update dependency versions.

--- project.clj
+++ project.clj
@@ -2,12 +2,12 @@
   :description "Source for my blog"
   :url "http://christianberg.github.io/"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [stasis "2.1.1"]
+                 [stasis "2.2.0"]
                  [ring "1.3.0"]
                  [hiccup "1.0.5"]
                  [me.raynes/cegdown "0.1.1"]
-                 [clj-time "0.7.0"]
-                 [org.clojure/data.xml "0.0.7"]]
+                 [clj-time "0.8.0"]
+                 [org.clojure/data.xml "0.0.8"]]
   :ring {:handler christianberg.web/app}
   :aliases {"build-site" ["run" "-m" christianberg.web/export]}
   :profiles {:dev {:plugins [[lein-ring "0.8.10"]]}})
