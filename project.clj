7c96f283fb8f22391cdfa824633d27839642263e
Christian Berg <berg.christian@gmail.com>
2014-09-08 23:41:19 +0200

    Update dependency: stasis 2.2.1

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "Source for my blog"
   :url "http://christianberg.github.io/"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [stasis "2.2.0"]
+                 [stasis "2.2.1"]
                  [ring "1.3.1"]
                  [hiccup "1.0.5"]
                  [me.raynes/cegdown "0.1.1"]
