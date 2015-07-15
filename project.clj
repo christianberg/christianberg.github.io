284537bed75817a8d066d0b3bf69d116b08ac24d
Christian Berg <berg.christian@gmail.com>
2015-07-15 22:12:30 +0200

    Update dependencies: Clojure 1.7, Ring 1.4

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject christianberg "0.1.0-SNAPSHOT"
   :description "Source for my blog"
   :url "http://christianberg.github.io/"
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [stasis "2.2.2"]
-                 [ring "1.3.2"]
+                 [ring "1.4.0"]
                  [hiccup "1.0.5"]
                  [me.raynes/cegdown "0.1.1"]
                  [clj-time "0.9.0"]
