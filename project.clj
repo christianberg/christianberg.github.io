2d5218b9f511f69b10fb0c0eb49400a5ff9ed0bc
Christian Berg <berg.christian@gmail.com>
2015-07-19 20:38:34 +0200

    Update dependency: clj-time 0.10

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
                  [ring "1.4.0"]
                  [hiccup "1.0.5"]
                  [me.raynes/cegdown "0.1.1"]
-                 [clj-time "0.9.0"]
+                 [clj-time "0.10.0"]
                  [org.clojure/data.xml "0.0.8"]]
   :ring {:handler christianberg.web/app}
   :aliases {"build-site" ["run" "-m" christianberg.web/export]}
